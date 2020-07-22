a8dd2650ba6541a0a3222758ddd63d69d0a06413
mokikiya <1831967524@qq.com>
2020-07-22 18:53:39 +0800

    完成底部导航条。包括：添加game，home，note三个对应的fragment和ViewModel和activity。添加menu、navigation、三个对应的图标。

---
+++ app/src/main/java/com/example/ai_wrongnote/GameViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.ai_wrongnote
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/example/ai_wrongnote/MainActivity.kt
+++ app/src/main/java/com/example/ai_wrongnote/MainActivity.kt
@@ -2,20 +2,24 @@ package com.example.ai_wrongnote
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 
-/*class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-}*/
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //绑定底部导航条
+
+
+
+
+        //启动连接数据库
         Thread(Runnable { DBConnection.link() }).start()
     }
 }
