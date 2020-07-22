e0718c22b26cd57a8206aa7d108b16d3a47d1a89
mokikiya <1831967524@qq.com>
2020-07-22 21:27:10 +0800

    mainActivity里实现底部导航条和标题对应（暂未完成）

--- app/src/main/java/com/example/ai_wrongnote/MainActivity.kt
+++ app/src/main/java/com/example/ai_wrongnote/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.ai_wrongnote
 
+
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,11 +16,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //绑定底部导航条
+        /*val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+        val appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.homeFragment,R.id.gameFragment,R.id.noteFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)*/
+
+       val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navController = Navigation.findNavController(this, R.id.fragment)
+        val configuration =
+            AppBarConfiguration.Builder(bottomNavigationView.menu).build()
+        NavigationUI.setupActionBarWithNavController(this, navController, configuration)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
 
 
 
 
         //启动连接数据库
-        Thread(Runnable { DBConnection.link() }).start()
+       // Thread(Runnable { DBConnection.link() }).start()
     }
 }
