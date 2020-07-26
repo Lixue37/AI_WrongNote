cc41ef06503af42e2ce12f690540cde8c72fea83
Lixue37 <Lixue3373@163.com>
2020-07-26 16:05:52 +0800

    拍照剪裁部分的ui(截取题干+截取图片)

---
+++ app/src/main/java/com/example/ai_wrongnote/GetAnswerActivity.kt
@@ -0,0 +1,14 @@
+package com.example.ai_wrongnote
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GetAnswerActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.get_answer_activity)
+
+    }
+}
