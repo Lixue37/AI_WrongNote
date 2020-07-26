7f5d871a253acb44af558daa6a8e838ce0f8bbe8
Lixue37 <Lixue3373@163.com>
2020-07-26 10:16:57 +0800

    Merge branch 'master' of D:\AndroidHomework\AI_WrongNote with conflicts.

--- app/src/main/java/com/example/ai_wrongnote/MainActivity.kt
+++ app/src/main/java/com/example/ai_wrongnote/MainActivity.kt
@@ -5,11 +5,8 @@ package com.example.ai_wrongnote
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
@@ -18,18 +15,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //绑定底部导航栏和顶部title
-        //获得BottomNavigationView
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-        //获得Navigation的Control
-        val navController = Navigation.findNavController(this, R.id.fragment_nav)
-        //绑定
-        NavigationUI.setupWithNavController(bottomNavigationView, navController)
-        //获取顶部appBar
+        //绑定底部导航条
+        /*val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+        val appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.homeFragment,R.id.gameFragment,R.id.noteFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)*/
+
+       val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navController = Navigation.findNavController(this, R.id.fragment_home)
         val configuration =
             AppBarConfiguration.Builder(bottomNavigationView.menu).build()
-        //绑定顶部title
         NavigationUI.setupActionBarWithNavController(this, navController, configuration)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
+
 
 
 
