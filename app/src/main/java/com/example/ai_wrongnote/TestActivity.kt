5df7b110ba1155db608b52098379b2b07660b05f
mokikiya <1831967524@qq.com>
2020-10-16 17:55:49 +0800

    testActivity中代码已改为尝试用于上传的代码，但暂未成功

--- app/src/main/java/com/example/ai_wrongnote/TestActivity.kt
+++ app/src/main/java/com/example/ai_wrongnote/TestActivity.kt
@@ -1,14 +1,17 @@
 package com.example.ai_wrongnote
 
+import android.R.attr.password
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.VolleyError
-import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.JsonRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_test.*
-import org.jetbrains.anko.support.v4.toast
-import org.jetbrains.anko.toast
+import org.json.JSONObject
 
 
 class TestActivity: AppCompatActivity() {
@@ -16,7 +19,7 @@ class TestActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
 
-        post_btn.setOnClickListener {
+       /* post_btn.setOnClickListener {
             val queue = Volley.newRequestQueue(this)
             var stringRequest = StringRequest(
                 "http://47.102.140.185:33060/",
@@ -31,24 +34,106 @@ class TestActivity: AppCompatActivity() {
             )
 
             queue.add(stringRequest)
+        }*/
+
+
+
+        //尝试上传
+        post_btn.setOnClickListener {
+            val requestQueue = Volley.newRequestQueue(this)
+
+
+            val hashMap = HashMap<String, String>()
+            hashMap["username"] = "Tom"
+            hashMap["password"] = ""
+
+            val jsonObjectRequest = CustomRequest(
+                Request.Method.POST,
+                "http://47.102.140.185:33060/",
+                hashMap,
+                Response.Listener { jsonObject -> Log.d("TagJson", jsonObject.toString()) },
+                Response.ErrorListener { })
+            requestQueue.add(jsonObjectRequest)
+
+            /*val map: MutableMap<String, String> = HashMap()
+            map["name1"] = "value1"
+            map["name2"] = "value2"
+            val jsonObject = JSONObject(params)
+            val jsonRequest: JsonRequest<JSONObject> =
+                object : JsonObjectRequest(Method.POST, httpurl, jsonObject,
+                    Response.Listener<JSONObject> { response -> Log.d(TAG, "response -> $response") },
+                    Response.ErrorListener { error -> Log.e(TAG, error.message, error) }) {
+                    //注意此处override的getParams()方法,在此处设置post需要提交的参数根本不起作用
+                    //必须象上面那样,构成JSONObject当做实参传入JsonObjectRequest对象里
+                    //所以这个方法在此处是不需要的
+                    //    @Override
+                    //    protected Map<String, String> getParams() {
+                    //          Map<String, String> map = new HashMap<String, String>();
+                    //            map.put("name1", "value1");
+                    //            map.put("name2", "value2");
+                    //        return params;
+                    //    }
+                    override fun getHeaders(): Map<String, String> {
+                        val headers = HashMap<String, String>()
+                        headers["Accept"] = "application/json"
+                        headers["Content-Type"] = "application/json; charset=UTF-8"
+                        return headers
+                    }
+                }
+            requestQueue.add(jsonRequest)*/
+
+           /* JsonObjectRequest jsonObjectRequest
+            JSONObject jsonObject=new JSONObject() ;
+
+            val merchant:MutableMap<String, String> = HashMap()
+            merchant.put("id", "id");
+            merchant.put("ncode", "ncode");
+            merchant.put("tradingName", "tradingName");
+
+            JSONObject jsonObject = new JSONObject(merchant);
+
+            Log.e(TAG, "getdata: " + jsonObject.toString());
+
+            JsonRequest<JSONObject> jsonRequest = new JsonObjectRequest(Request.Method.POST,
+                "",
+                jsonObject,
+                new Response . Listener < JSONObject >() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, "response -> " + response.toString());
+                    }
+                },
+                new Response . ErrorListener () {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e(TAG, error.getMessage(), error);
+                    }
+                }) {
+
+                @Override
+                public Map<String, String> getHeaders() {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Accept", "application/json");
+                headers.put("Content-Type", "application/json; charset=UTF-8");
+
+                return headers;
+            }
+            };
+            requestQueue.add(jsonRequest);*/
+
+
         }
+
+
+        }
+
+
+
+
 
     }
 
-    //val id = 1
 
 
 
-/*
-* object : Listener<String?>() {
-            fun onResponse(response: String?) {
-                Log.d("TAG", response)
-            }
-        }, object : ErrorListener() {
-            fun onErrorResponse(error: VolleyError) {
-                Log.e("TAG", error.message, error)
-            }
-        }
-* */
-}
 
