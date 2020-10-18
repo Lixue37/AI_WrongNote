d8ca707f05f780156de572c6440edd27525155eb
mokikiya <1831967524@qq.com>
2020-10-18 13:48:41 +0800

    CommendDetailActivity中解决了所有可能由“NULL”导致异常的地方

--- app/src/main/java/com/example/ai_wrongnote/activity/CommendDetailActivity.kt
+++ app/src/main/java/com/example/ai_wrongnote/activity/CommendDetailActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ai_wrongnote.R
 import kotlinx.android.synthetic.main.activity_commend.*
+import org.jetbrains.anko.image
 import org.jetbrains.anko.toast
 
 
@@ -38,25 +39,57 @@ class CommendDetailActivity : AppCompatActivity() {
         commend_data_actvt_text.text = commend_data
         //题干补充图片的显示（暂且将读出的图片名绑定道文本框中。后续可以根据名称绑定相应图片 ！！！！！！ 布局中图片的命名为commend_data_image_actvt
         val commend_data_image = intent.getStringExtra("commend_data_image")
-        commend_data_image_txt.text = commend_data_image
+        if (commend_data_image=="0"){
+            commend_data_image_txt.text ="暂无图片"
+            //此处无需绑定默认图片
+        }
+        else{
+            commend_data_image_txt.text = commend_data_image
+            //预留了绑定图片的位置
+
+        }
+
         //知识点文本框显示
         val know_point = intent.getStringExtra("know_point")
-        commend_point_data_actvt_text.text = know_point
+        /*处理数据库中数据为空的问题。在转成json时，就已经将string类型的空数据转成 "0" 将double类型的数据转成 0.0 */
+        if (know_point=="0")
+            commend_point_data_actvt_text.text ="暂无标签"
+        else
+            commend_point_data_actvt_text.text = know_point
+
         //难度文本框显示
-        var how_hard = intent.getStringExtra("how_hard")
-        if (how_hard==null) how_hard="-1.0"
-        commend_how_hard_data_text.text = how_hard
+        val how_hard = intent.getStringExtra("how_hard")
+        if (how_hard=="0.0")
+            commend_how_hard_data_text.text = "-1.0"
+        else
+            commend_how_hard_data_text.text = how_hard
+
         //答案文本框显示
-        var answer = intent.getStringExtra("answer")
-        if (answer=="0")   answer="暂无答案"
-        commend_answer_text.text = answer
+        val answer = intent.getStringExtra("answer")
+        if (answer=="0")
+            commend_answer_text.text = "暂无答案"
+        else
+            commend_answer_text.text = answer
+
         //答案详解文本框显示
-        var answer_detail = intent.getStringExtra("answer_detail")
-        if (answer_detail=="0")   answer_detail="暂无详解"
-        answer_detail_actvt_text.text = answer_detail
+        val answer_detail = intent.getStringExtra("answer_detail")
+        if (answer_detail=="0")
+            answer_detail_actvt_text.text ="暂无详解"
+        else
+            answer_detail_actvt_text.text = answer_detail
+
         //答案详解图片显示（暂且将读出的图片名绑定道文本框中。后续可以根据名称绑定相应图片 ！！！！！！！布局中图片的命名为answer_detail_image_actvt
         val answer_detail_image = intent.getStringExtra("answer_detail_image")
-        answer_detail_image_txt.text = answer_detail_image
+        if (answer_detail_image=="0"){
+            answer_detail_image_txt.text ="暂无图片"
+            //此处无需绑定默认图片
+        }
+        else{
+            answer_detail_image_txt.text = answer_detail_image
+            //预留了绑定图片的位置
+
+        }
+
 
 
         /*测试能否拿到数据*/
